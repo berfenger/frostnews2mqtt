e1eec74c4e840ac0b665764d443e614abca22732
Arturo Casal <berfenger@gmail.com>
2025-11-21 18:28:47 +0100

    fix: race conditions reading smart meter registers

---
+++ pkg/sunspec_modbus/encoding.go
@@ -0,0 +1,45 @@
+package sunspec_modbus
+
+import (
+	"encoding/binary"
+
+	"github.com/simonvetter/modbus"
+)
+
+func bytesToUint32s(endianness modbus.Endianness, wordOrder modbus.WordOrder, in []byte) (out []uint32) {
+	var u32 uint32
+
+	for i := 0; i < len(in); i += 4 {
+		switch endianness {
+		case modbus.BIG_ENDIAN:
+			if wordOrder == modbus.HIGH_WORD_FIRST {
+				u32 = binary.BigEndian.Uint32(in[i : i+4])
+			} else {
+				u32 = binary.BigEndian.Uint32(
+					[]byte{in[i+2], in[i+3], in[i+0], in[i+1]})
+			}
+		case modbus.LITTLE_ENDIAN:
+			if wordOrder == modbus.LOW_WORD_FIRST {
+				u32 = binary.LittleEndian.Uint32(in[i : i+4])
+			} else {
+				u32 = binary.LittleEndian.Uint32(
+					[]byte{in[i+2], in[i+3], in[i+0], in[i+1]})
+			}
+		}
+
+		out = append(out, u32)
+	}
+
+	return
+}
+
+func bytesToUint16(endianness modbus.Endianness, in []byte) (out uint16) {
+	switch endianness {
+	case modbus.BIG_ENDIAN:
+		out = binary.BigEndian.Uint16(in)
+	case modbus.LITTLE_ENDIAN:
+		out = binary.LittleEndian.Uint16(in)
+	}
+
+	return
+}
